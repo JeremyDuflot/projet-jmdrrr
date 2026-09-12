43de1d4aba43c92f8dce7c1de838fc07d9168daa
Mickael-Desclaux <mickaeldesclaux@gmail.com>
2026-09-12 17:48:37 +0200

    added first launch seed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '@/css/style.css'
 import App from './App.vue'
 import router from './router'
 import { persistPlugin } from '@/stores/plugins/persist'
+import { readStorage } from '@/utils/storage'
 
 const app = createApp(App)
 
@@ -12,5 +13,10 @@ pinia.use(persistPlugin)
 
 app.use(pinia)
 app.use(router)
+
+if (readStorage('campaigns') === null) {
+  const { seedCampaigns } = await import('@/data/seed')
+  seedCampaigns(pinia)
+}
 
 app.mount('#app')
